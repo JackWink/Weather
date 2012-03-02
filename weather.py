e1509e0d9e7ac20adefcd9e1654700aac02a5368
Jack Wink <jackwink@umich.edu>
2012-03-01 21:03:24 -0500

    Added support for metric/english units as an option

--- weather.py
+++ weather.py
@@ -2,13 +2,20 @@
 import requests, argparse, sys, json 
 
 settings = {
-    "api_key": "your-api-key"    
+    "api_key": "your-api-key",   
+    "metric": False 
 }
 
-
+## get_max_width
+#
+#  Returns the max width of any row in an array
 def get_max_width(table, i):
     return max([len(row[i]) for row in table])
 
+## print_table
+#
+#  Aligns and prints an array into a table, given that the first 
+#  row in the array contains the column names
 def print_table(out, table):
     col_paddings = []
     for i in range(len(table[0])):
@@ -33,13 +40,17 @@ def print_table(out, table):
         print >> out
 
 
-
+## Make_str
+#
+#  Takes the options and arguments and assembles the query string
 def make_str(args, options):
     url = ""
+
+    # In the case no options are set, use the default
     if not (args.now or args.hourly or args.alerts or args.forecast):
         args.now = True
         args.alerts = True
-    
+
     if (args.now):
         url += options['now']
     if (args.hourly):
@@ -48,16 +59,21 @@ def make_str(args, options):
         url += options['forecast']
     if (args.alerts):
         url += options['alerts']
+    
     return url
 
 
-
+## parse_alerts
+#
+#  Takes the returned data and parses the alert messages
 def parse_alerts(data):
     for alert in range(len(data['alerts'])):
         print "\033[91m" + data['alerts'][alert] + "\033[0m"
 
 
-
+## parse_conditions
+#
+#  Parses the current conditions from the API
 def parse_conditions(data):
     print "Weather for " + data['display_location']['full']
     print "Currently: "  + data['temperature_string'] + " " + data['weather'] 
@@ -65,32 +81,58 @@ def parse_conditions(data):
     print "Humidity: "   + data['relative_humidity']
 
 
-
-def parse_hourly(data):
+## parse_hourly
+#
+#  Parses the hourly condition data from the API
+def parse_hourly(data, metric):
+    # Need to generate an array to send the print_table, first row must be the keys
     val = []
-    val.append(["Date", "Hour", "Temp (F / C)", "Weather"])
+    val.append(["Date", "Hour", "Temperature", "Weather"])
+    
     for item in data:
+        # Format the date and temp strings before appending to the array
         time = item["FCTTIME"]
         date = time["mon_abbrev"] + " " + time["mday_padded"] + ", " + time["year"]
-        temp = item["temp"]["english"] + " / " + item["temp"]["metric"]
+        
+        if settings['metric'] or metric:
+            temp = item["temp"]["metric"] + u" \u00B0C"
+        else:
+            temp = item["temp"]["english"] + u" \u00B0F"
         val.append([date, time['civil'], temp, item['condition']])
+    
     print "\n36 Hour Hourly Forecast:"
     print_table(sys.stdout, val)
 
-def parse_forecast(data):
+
+## parse_forecast
+#
+#  Parses the forecast data from the API
+def parse_forecast(data, metric):
+    # Need to generate an array to send the print_table, first row must be the keys
     val = []
     val.append(["Date", "Condition", "Temp (Hi/Lo)", "Wind", "Humidity"])
+    
     for item in data:
         date = item['date']
         date_str = date['monthname'] + " " + str(date['day']) + ", " + str(date['year'])
-        temp = item['high']['fahrenheit'] + "F (" + item['high']['celsius'] + "C) / " + item['low']['fahrenheit'] + "F (" + item['low']['celsius'] + "C)"
-        wind = "~" + str(item['avewind']['mph']) + "mph (" + str(item['avewind']['kph']) + "kph) " + item['avewind']['dir'] 
+        
+        if settings['metric'] or metric:
+            temp = item['high']['celsius'] + u" \u00B0C / " + item['low']['celsius'] + u" \u00B0C"
+            wind = "~" + str(item['avewind']['kph']) + "kph " + item['avewind']['dir'] 
+        else:
+            temp = item['high']['fahrenheit'] + u" \u00B0F / " + item['low']['fahrenheit'] + u" \u00B0F"
+            wind = "~" + str(item['avewind']['mph']) + "mph " + item['avewind']['dir'] 
+
         hum = str(item["avehumidity"]) + "%"
         val.append([date_str, item['conditions'], temp, wind, hum])
+    
     print "\nWeather Forecast:"
     print_table(sys.stdout, val)
 
 
+## parse_weather_data
+#
+#  Parses the returned API data along with the options for proper formatting
 def parse_weather_data(data, args):
     data = json.loads(data)
     
@@ -109,33 +151,35 @@ def parse_weather_data(data, args):
     if args.now:
         parse_conditions(data['current_observation'])
     if args.hourly:
-        parse_hourly(data['hourly_forecast'])
+        parse_hourly(data['hourly_forecast'], args.metric)
     if args.forecast:
-        parse_forecast(data['forecast']['simpleforecast']['forecastday'])
+        parse_forecast(data['forecast']['simpleforecast']['forecastday'], args.metric)
 
 
 def main(args):
-    # api/KEY/FEATURE/[FEATURE].../q/QUERY.FORMAT
-    base_url="http://api.wunderground.com/api/%s/" % settings['api_key']
-    query="q/%s.json"
-
-    options = {
+    # API methods 
+    values = {
         "now": "conditions/",
         "forecast": "forecast/",
         "hourly": "hourly/",
         "alerts": "alerts/",
         "ip": "autoip"
     }
-     
+    
+    # Create a location string, or use geoip
+    query="q/%s.json"
     if args.location:
         query = query % "_".join(args.location);
     else:
-        query = query % options['ip']
+        query = query % values['ip']
     
-    url = base_url + make_str(args, options) + query
+    base_url="http://api.wunderground.com/api/%s/" % settings['api_key']
+    url = base_url + make_str(args, values) + query
+    
+    # Make the API request, parse the data
     r = requests.get(url)
-
     parse_weather_data(r.content, args)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Display the current weather, or forecast")
@@ -144,6 +188,10 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--forecast', help='Get the current forecast', action='store_true')
     parser.add_argument('-o', '--hourly', help='Get the hourly forecast', action='store_true')
     parser.add_argument('-a', '--alerts', help='View any current weather alerts (Default)', action='store_true')
+    if settings['metric']:
+        parser.add_argument('-m', '--metric', help='Use metric units instead of English units (Default)', action='store_true')
+    else:
+        parser.add_argument('-m', '--metric', help='Use metric units instead of English units', action='store_true')
     
     main(parser.parse_args())
 
