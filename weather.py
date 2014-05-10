bd7135bedd897069b2250c5639f2e1027bc1c22c
Jack Wink <jackwink@umich.edu>
2014-05-09 21:25:07 -0400

    clean up some of the printing functionality

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 import requests
 import argparse
 import sys
@@ -20,12 +21,12 @@ class Settings(object):
         if not Settings.settings:
             Settings.file_path = os.path.expanduser(WEATHER_CONF_FILE)
             if not os.path.exists(Settings.file_path):
-                self.generate_default_weatherrc()
+                self._generate_default_weatherrc()
 
             with open(Settings.file_path) as weatherrc:
                 Settings.settings = json.load(weatherrc)
 
-    def generate_default_weatherrc(self):
+    def _generate_default_weatherrc(self):
         """
         Writes a default weather conf file
         """
@@ -41,114 +42,167 @@ class Settings(object):
 
 
 
-def get_max_col_width(table, column_index):
+class ResultPrinter(object):
     """
-    Returns the length of the longest string in any column
+    Responsible for printing weather underground API results
+    in a formatted manner.
     """
-    return max([len(row[column_index]) for row in table])
 
-def print_table(out, table):
-    """
-    Aligns and prints an array into a formatted table
+    def __init__(self, out=None):
+        if not out:
+            # Wrap sys.stdout in a utf8 stream writer in case output is piped
+            out = codecs.getwriter('utf8')(sys.stdout)
 
-    Requires that the first row in the array contain the column names
-    and that each row contains the same number of elements
-    """
-    max_col_widths = []
-    for col in range(len(table[0])):
-        max_col_widths.append(get_max_col_width(table, col))
-
-    print >> out, table[0][0].ljust(max_col_widths[0] + 1),
-    for i in range(1, len(table[0])):
-        col = table[0][i].rjust(max_col_widths[i] + 2)
-        out.write(col)
+        self.out      = out
+        self.settings = Settings()
 
 
-    print >> out, ""
-    print >> out, "-" * (sum(max_col_widths) + 3 * len(max_col_widths))
-
-    table.pop(0)
-    for row in table:
-        print >> out, row[0].ljust(max_col_widths[0] + 1),
-        for i in range(1, len(row)):
-            col = row[i].rjust(max_col_widths[i] + 2)
-            out.write(col)
-
-        print >> out
+    def print_alerts(self, data):
+        """
+        Prints any weather alerts in red
+        """
+        for alert in data['alerts']:
+            print >> self.out, "\033[91m" + alert['message'].rstrip("\n") + "\nExpires: " + alert['expires'] + "\033[0m"
 
 
-def print_alerts(data):
-    """
-    Prints any weather alerts in red
-    """
-    for alert in data['alerts']:
-        print "\033[91m" + alert['message'].rstrip("\n") + "\nExpires: " + alert['expires'] + "\033[0m"
+    def print_conditions(self, data):
+        """
+        Prints the current weather conditions
+        """
+        print >> self.out, "Weather for " + data['display_location']['full']
+        print >> self.out, "Currently: "  + data['temperature_string'] + " " + data['weather']
+        print >> self.out, "Wind: "       + data['wind_string']
+        print >> self.out, "Humidity: "   + data['relative_humidity']
 
 
-def print_conditions(data):
-    """
-    Prints the current weather conditions
-    """
-    print "Weather for " + data['display_location']['full']
-    print "Currently: "  + data['temperature_string'] + " " + data['weather']
-    print "Wind: "       + data['wind_string']
-    print "Humidity: "   + data['relative_humidity']
+    def print_hourly(self, data):
+        """
+        Prints the hourly weather data in a table
+        """
+        # Need to generate an array to send the print_table, first row must be the keys
+        val = []
+        val.append(["Date", "Hour", "Temperature", "Chance of Rain", "Weather"])
+
+        for item in data:
+            # Format the date and temp strings before appending to the array
+            time = item["FCTTIME"]
+            date = self._format_date(time["mon_abbrev"], time["mday_padded"], time["year"])
+            temp = self._format_degree(item["temp"])
+            val.append([date, time['civil'], temp,  item["pop"] + "%", item['condition']])
+
+        print >> self.out, "36 Hour Hourly Forecast:"
+        self._print_table(val)
 
 
-def print_hourly(data, metric):
-    """
-    Prints the hourly weather data in a table
-    """
-    settings = Settings()
-    # Need to generate an array to send the print_table, first row must be the keys
-    val = []
-    val.append(["Date", "Hour", "Temperature", "Chance of Rain", "Weather"])
+    def print_forecast(self, data):
+        """
+        Prints the 3 day forcast data in a table
+        """
+        val = []
+        # Need to generate an array to send the print_table, first row must be the keys
+        val.append(["Date", "Condition", "Chance of Rain", "Temp (Hi/Lo)", "Wind", "Humidity"])
 
-    for item in data:
-        # Format the date and temp strings before appending to the array
-        time = item["FCTTIME"]
-        date = time["mon_abbrev"] + " " + time["mday_padded"] + ", " + time["year"]
+        for item in data:
+            date = item['date']
+            date_str = self._format_date(date['monthname'], date['day'], date['year'])
+            temp     = self._format_degree(item['high']) + " / " + self._format_degree(item['low'])
+            wind     = self._format_windspeed(item['avewind'])
 
-        if settings.metric or metric:
-            temp = item["temp"]["metric"] + u" \u00B0C"
+            hum = str(item["avehumidity"]) + "%"
+            val.append([date_str, item['conditions'], str(item["pop"]) + "%", temp, wind, hum])
+
+        print >> self.out, "Weather Forecast:"
+        self._print_table(val)
+
+
+
+
+    def _get_max_col_width(self, table, column_index):
+        """
+        Returns the length of the longest string in any column
+        """
+        return max([len(row[column_index]) for row in table])
+
+    def _print_table(self, table):
+        """
+        Aligns and prints an array into a formatted table
+
+        Requires that the first row in the array contain the column names
+        and that each row contains the same number of elements
+        """
+        max_col_widths = []
+        for col in range(len(table[0])):
+            max_col_widths.append(self._get_max_col_width(table, col))
+
+        print >> self.out, table[0][0].ljust(max_col_widths[0] + 1),
+        for i in range(1, len(table[0])):
+            col = table[0][i].rjust(max_col_widths[i] + 2)
+            self.out.write(col)
+
+
+        print >> self.out, ""
+        print >> self.out, "-" * (sum(max_col_widths) + 3 * len(max_col_widths))
+
+        table.pop(0)
+        for row in table:
+            print >> self.out, row[0].ljust(max_col_widths[0] + 1),
+            for i in range(1, len(row)):
+                col = row[i].rjust(max_col_widths[i] + 2)
+                self.out.write(col)
+
+            print >> self.out, ""
+
+
+    def _format_degree(self, temp_dict):
+        """
+        Takes a dictionary from the Weather underground api
+        and returns a formatted temperature string ex: "62 °F", "25 °C"
+        """
+        degree = u"\u00B0"
+
+        temp = None
+        if self.settings.metric:
+            if "celsius" in temp_dict:
+                temp = temp_dict["celsius"]
+            elif "metric" in temp_dict:
+                temp = temp_dict["metric"]
         else:
-            temp = item["temp"]["english"] + u" \u00B0F"
-        val.append([date, time['civil'], temp,  item["pop"] + "%", item['condition']])
+            if "fahrenheit" in temp_dict:
+                temp = temp_dict["fahrenheit"]
+            elif "english" in temp_dict:
+                temp = temp_dict["english"]
 
-    print "\n36 Hour Hourly Forecast:"
-    print_table(sys.stdout, val)
+        # For whatever reason, weather underground returns temps
+        # as both strings and ints
+        temp = "{0:3}".format(str(temp)) + degree
 
-
-def print_forecast(data, metric):
-    """
-    Prints the 3 day forcast data in a table
-    """
-    settings = Settings()
-    # Need to generate an array to send the print_table, first row must be the keys
-    val = []
-    val.append(["Date", "Condition", "Chance of Rain", "Temp (Hi/Lo)", "Wind", "Humidity"])
-
-    for item in data:
-        date = item['date']
-        date_str = date['monthname'] + " " + str(date['day']) + ", " + str(date['year'])
-
-        if settings.metric or metric:
-            temp = item['high']['celsius'] + u" \u00B0C / " + item['low']['celsius'] + u" \u00B0C"
-            wind = "~" + str(item['avewind']['kph']) + "kph " + item['avewind']['dir']
+        if self.settings.metric:
+            return temp + "C"
         else:
-            temp = item['high']['fahrenheit'] + u" \u00B0F / " + item['low']['fahrenheit'] + u" \u00B0F"
-            wind = "~" + str(item['avewind']['mph']) + "mph " + item['avewind']['dir']
+            return temp + "F"
 
-        hum = str(item["avehumidity"]) + "%"
-        val.append([date_str, item['conditions'], str(item["pop"]) + "%", temp, wind, hum])
+    def _format_windspeed(self, windspeed_dict):
+        """
+        Returns a formatted windspeed
+        """
+        windspeed = None
+        if self.settings.metric:
+            windspeed = str(windspeed_dict['kph']) + "kph"
+        else:
+            windspeed = str(windspeed_dict['mph']) + "mph"
 
-    print "\nWeather Forecast:"
-    print_table(sys.stdout, val)
+        return "~{0:5} {1:3}".format(windspeed, windspeed_dict['dir'])
+
+    def _format_date(self, month, day, year):
+        """
+        Returns a formatted date string
+        """
+        return "{0} {1:3} {2:4}".format(month, str(day)+",", str(year))
 
 
 def print_weather_data(data, args):
     """
-    Prints the supplied weather data as specified by the options and arguments.
+    Prints the supplied weather data as specified by the options and program arguments.
     """
     data = json.loads(data)
 
@@ -162,15 +216,15 @@ def print_weather_data(data, args):
             print result['name'] + ", " + result['state'] + " " + result['country_name']
         return
 
+    result_printer = ResultPrinter()
     if args.alerts:
-        print_alerts(data)
+        result_printer.print_alerts(data)
     if args.now:
-        print_conditions(data['current_observation'])
+        result_printer.print_conditions(data['current_observation'])
     if args.hourly:
-        print_hourly(data['hourly_forecast'], args.metric)
+        result_printer.print_hourly(data['hourly_forecast'])
     if args.forecast:
-        print_forecast(data['forecast']['simpleforecast']['forecastday'], args.metric)
-
+        result_printer.print_forecast(data['forecast']['simpleforecast']['forecastday'])
 
 
 def make_query_path(args):
@@ -226,9 +280,6 @@ def make_api_url(args):
 def main(args):
     api_url = make_api_url(args)
     r = requests.get(api_url)
-
-    # Wrap sys.stdout in a utf8 stream writer in case output is piped
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
     print_weather_data(r.content, args)
 
