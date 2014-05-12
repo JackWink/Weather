d0a07e993b800ce6a8b99ee6c5f24ce05f602864
Jack Wink <jackwink@umich.edu>
2014-05-12 00:05:31 -0400

    add file loading test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -10,10 +10,16 @@ class TestSettingsFunctions(unittest.TestCase):
         if os.path.exists(self.conf_path):
             self._backup_settings(self.conf_path)
 
+        #XXX - Need to reset internal settings so it'll reparse the conf file
+        if weather.Settings.settings:
+            weather.Settings.settings = None
+
+
     def tearDown(self):
         backupPath= os.path.expanduser("~/.weatherrc_backup")
         if os.path.exists(backupPath):
             self._restore_settings(self.conf_path)
+
 
     def _backup_settings(self, path):
         backup = os.path.expanduser("~/.weatherrc_backup")
@@ -25,7 +31,16 @@ class TestSettingsFunctions(unittest.TestCase):
         shutil.copy(backup, path)
         os.remove(backup)
 
+    def _write_conf(self, api_key, metric_str):
+        with open(self.conf_path, 'w') as f:
+            f.write('{ "api_key": "%s", "metric": %s }' % (api_key, metric_str))
+
+
     def test_default_file_creation(self):
+        """
+        Test that the default conf file is a valid JSON file that contains both
+        the metric and API key value pair.
+        """
         self.assertFalse(os.path.exists(self.conf_path))
         s = weather.Settings()
         self.assertTrue(os.path.exists(self.conf_path))
@@ -35,6 +50,24 @@ class TestSettingsFunctions(unittest.TestCase):
 
         self.assertFalse(data["metric"])
         self.assertEqual(data["api_key"], "your-api-key")
+
+    def test_file_loading(self):
+        """
+        Test that the loading Settings overrides the default conf file
+        """
+
+        self._write_conf("12341234", "true")
+        s = weather.Settings()
+        self.assertTrue(s.metric)
+        self.assertEqual(s.api_key, "12341234")
+
+        self._write_conf("1234", "false")
+        #XXX reset conf file
+        weather.Settings.settings = None
+        s = weather.Settings()
+        self.assertFalse(s.metric)
+        self.assertEqual(s.api_key, "1234")
+
 
 
 if __name__ == "__main__":
