95104c0706ca1282b8050fe7bf24e81ae854bfc2
Jack Wink <jackwink@umich.edu>
2014-05-17 15:04:45 -0400

    add test stubs for api related functions

---
+++ tests/test_api.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestApiRequests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_make_query_path(self):
+        pass
+
+    def test_make_api_url(self):
+        pass
+
+    def test_main(self):
+        pass
